93b6f291bd7d3b52d274e84756fbc08cae0fcb4b
Zbook <thth.btx@outlook.com>
2023-02-05 18:13:56 +0700

    change java file

--- install-java.sh
+++ install-java.sh
@@ -4,7 +4,9 @@ cd /opt/java || exit
 javaZuluName="zulu11.62.17-ca-jdk11.0.18-linux_aarch32hf"
 curl -fsSL "https://cdn.azul.com/zulu-embedded/bin/${javaZuluName}.tar.gz" -o $javaZuluName.tar.gz
 tar -xzvf $javaZuluName.tar.gz
+echo remove tar file
 rm $javaZuluName.tar.gz
+echo changing folder
 mv $javaZuluName openjdk11
 # java path will create symlink: /usr/bin/java --> /etc/alternatives/java
 # update-alternatives --install /usr/bin/java java /opt/java/openjdk/bin/java 1
